22e7553e09b2ea1891c438fefc95a1f2230e8ce5
JakeVz <jakevz122@gmail.com>
2016-02-26 02:38:29 -0800

    fixed the stuff in DamageCalc

--- DamageCalc.java
+++ DamageCalc.java
@@ -1,7 +1,6 @@
 public class DamageCalc {
-   public int basicCalc() {
-      return 5;
-   }
+   private int damage;
+   private int chanceEvasion;
 
 
    /*
@@ -11,9 +10,9 @@ public class DamageCalc {
 
       calc the amount of damage that will be put on a fighter
    */
-   public int totalDamage(int attackType, int evasion){
+   public void attackDamage(int attackType){
       int damage = 0;
-      int chance = 0;
+      //int chance = 0;
       
       //ATTACK TYPE 1
       if (attackType == 1){ //damage will be 1-5
@@ -32,42 +31,45 @@ public class DamageCalc {
          int range = (15 - 10) + 1;
          damage = (int)(Math.random() * range) + 10; //damage wil be 5 - 10
       }
+   }
 
-
-
+   public void evasionChance(int evasion){
+      int chance = 0;
       if (evasion == 0){ // miss chance = 1 in 50
          chance = (int)(Math.random() * 50) + 1;
          if (chance == 1)
-            damage = 0;
+            chanceEvasion = 0;
       }
       if (evasion >= 1 && evasion < 25){ // miss chance = 1 in 20
          chance = (int)(Math.random() * 20) + 1;
          if (chance == 1)
-            damage = 0;
+            chanceEvasion = 0; 
          
       }
       if (evasion >= 25 && evasion < 50){ // miss chance = 1 in 15
          chance = (int)(Math.random() * 15) + 1;
          if (chance == 1)
-            damage = 0;
+            chanceEvasion = 0;
          
       }
       if (evasion >= 50 && evasion < 75){ // miss chance = 1 in 10
          chance = (int)(Math.random() * 10) + 1;
          if (chance == 1)
-            damage = 0;
+            chanceEvasion = 0;
       
       }
       if (evasion >= 75 && evasion < 100){ // miss chance = 1 in 5
          chance = (int)(Math.random() * 5) + 1;
          if (chance == 1)
-            damage = 0;
+            chanceEvasion = 0;
       
       }
       if (evasion == 100){ // miss chnace = 1 in 1
-         damage = 0;
+         chanceEvasion = 0;
       }
+   }
 
-   return damage;
+   public int totalDamage(){
+      return damage * chanceEvasion;
    }
 }
--- TwoHandSword.java
+++ TwoHandSword.java
@@ -1,5 +1,15 @@
 public class TwoHandSword extends Weapon {
+   private DamageCalc Calc = new DamageCalc();
+   
    public TwoHandSword() {
       super(50, 20);
    }
+
+/*
+   public int totalDamage(int attackType){
+      Calc.totalDamage(attackType
+
+   }
+
+*/
 }
