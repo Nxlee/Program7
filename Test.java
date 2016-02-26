e7f033d84eb79f32f3630da6e1294315d40a6c8f
Nxlee <nxlee38@gmail.com>
2016-02-25 22:44:19 -0800

    Made jake gay

--- Test.java
+++ Test.java
@@ -3,4 +3,12 @@ public class Test {
    public int gay(){
       return 69;
    }
+
+   public boolean isJakeGay() {
+      if(gay() == 69) {
+         return true;
+      } else {
+         return true;
+      }
+   }
 }
