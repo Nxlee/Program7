176bb98cb2d7344cca2c3dfa732b87958ca4e7be
Nxlee <nxlee38@gmail.com>
2016-03-08 17:24:51 -0800

    Added skeleton for game driver

---
+++ Play.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Play {
+   public static void main(String[] args) {
+      int gamesPlayed = 0;
+      int gamesToPlay = 3;
+      boolean passed;
+      while(gamesPlayed < gamesToPlay) {
+         passed = false;
+         while(true) {
+            //in shop. If key is pressed, break
+            break;
+         }
+         while(!passed) {
+            //play against AI, once you win, passed = true.
+         }
+      }
+   }
+}
